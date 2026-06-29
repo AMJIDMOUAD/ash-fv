8de24955c708bc0c0dd9f98e5fefa0a7eda182ff
AMJIDMOUAD <amjidmouad19@gmail.com>
2026-06-29 22:21:33 +0100

    fix: manual URL parsing + add ping function

--- netlify/functions/send-audit.js
+++ netlify/functions/send-audit.js
@@ -1,5 +1,4 @@
 const nodemailer = require("nodemailer");
-const qs = require("querystring");
 
 exports.handler = async function (event) {
   if (event.httpMethod !== "POST") {
@@ -7,12 +6,24 @@ exports.handler = async function (event) {
   }
 
   try {
-    const body = event.body || "";
-    const data = qs.parse(body);
-    const name = data.name || "";
-    const business = data.business || "";
-    const emailAddr = data.email || "";
-    const phone = data.phone || "";
+    let name = "", business = "", emailAddr = "", phone = "";
+
+    if (event.headers["content-type"] === "application/json") {
+      const d = JSON.parse(event.body);
+      name = d.name || "";
+      business = d.business || "";
+      emailAddr = d.email || "";
+      phone = d.phone || "";
+    } else {
+      const parts = (event.body || "").split("&");
+      for (const p of parts) {
+        const [k, v] = p.split("=").map(decodeURIComponent);
+        if (k === "name") name = v || "";
+        else if (k === "business") business = v || "";
+        else if (k === "email") emailAddr = v || "";
+        else if (k === "phone") phone = v || "";
+      }
+    }
 
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
@@ -28,12 +39,7 @@ exports.handler = async function (event) {
       from: "ash8518@gmail.com",
       to: "ash8518@gmail.com",
       subject: "New Free Audit Request - " + name + " (" + business + ")",
-      text:
-        "New Free Audit Request\n\n"
-        + "Name: " + name + "\n"
-        + "Business: " + business + "\n"
-        + "Email: " + emailAddr + "\n"
-        + "Phone: " + phone + "\n",
+      text: "New Free Audit Request\n\nName: " + name + "\nBusiness: " + business + "\nEmail: " + emailAddr + "\nPhone: " + phone + "\n",
     });
 
     return {
